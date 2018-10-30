5b9551fc601d6cec978fd792cd7c0058f830a18d
anton1gritsenko <7XpressMusic5310>
2018-10-30 17:54:04 +0200

    commin one

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $('.bxslider').bxSlider({
+//     minSlides: 3,
+//     maxSlides: 2,
+//     slideWidth: 185,
+//     slideMargin: 20,
+//     auto: true,
+// });
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+   stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 600
+});
